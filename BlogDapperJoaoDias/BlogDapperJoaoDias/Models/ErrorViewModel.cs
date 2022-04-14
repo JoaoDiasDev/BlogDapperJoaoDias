de2e6e04941000504981eb99fcb568d3513d3d8b
JoaoDias <joaodiasworking@gmail.com>
2022-04-13 23:44:35 -0300

    Iniciando projeto de Blog usando Dapper

---
+++ BlogDapperJoaoDias/BlogDapperJoaoDias/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BlogDapperJoaoDias.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
