4f6aede7e823f2f1f8f671d25985531cf2cb3a04
JoaoDias <joaodiasworking@gmail.com>
2022-05-23 22:49:29 -0300

    Integrando com Dapper, criando entitades

---
+++ BlogDapperJoaoDias/BlogDapperJoaoDias/Services/ConnectionService.cs
@@ -0,0 +1,39 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace BlogDapperJoaoDias.Services
+{
+    public class ConnectionService
+    {
+        private SqlConnection _myConnection;
+        private IConfiguration _configuration;
+
+        public ConnectionService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            var connectionString = _configuration.GetConnectionString("DefaultConnection").ToString();
+            _myConnection = new SqlConnection(connectionString);
+            if (_myConnection.State != ConnectionState.Open)
+            {
+                _myConnection.Open();
+            }
+        }
+
+        internal SqlConnection DbConnection()
+        {
+            return _myConnection;
+        }
+
+        internal SqlConnection ForDapper()
+        {
+            var connection = new SqlConnection(_configuration.GetConnectionString("DefaultConnection").ToString());
+            var state = connection.State;
+            if (state != ConnectionState.Open)
+            {
+                connection.Open();
+            }
+
+            return connection;
+        }
+    }
+}
