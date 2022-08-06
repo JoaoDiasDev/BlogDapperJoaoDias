9cc7f7e3f662e354c8214767bbc9245fc987f60d
JoaoDias <joaodiasworking@gmail.com>
2022-08-06 15:41:21 -0300

    Implementando Controller de Comentarios e metodos para ser possivel comentar nos posts

---
+++ BlogDapperJoaoDias/BlogDapperJoaoDias/Controllers/CommentController.cs
@@ -0,0 +1,47 @@
+﻿using BlogDapperJoaoDias.Entities;
+using BlogDapperJoaoDias.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogDapperJoaoDias.Controllers
+{
+    public class CommentController : Controller
+    {
+        private CommentService _commentService;
+        private ArticleService _articleService;
+
+        public CommentController(IServiceProvider serviceProvider)
+        {
+            _commentService = serviceProvider.GetRequiredService<CommentService>();
+            _articleService = serviceProvider.GetRequiredService<ArticleService>();
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Add(string id, IFormCollection form)
+        {
+            var article = _articleService.GetByGuid(id);
+            var comment = new Comment
+            {
+                ArticleId = article.ArticleId,
+                CommentText = form["message"],
+                Email = form["email"],
+                Name = form["name"],
+                CreatedDate = DateTime.Now,
+                ModifiedDate = DateTime.Now,
+                Status = 1
+            };
+
+            var result = _commentService.Add(comment);
+            if (result > 0)
+            {
+                return Ok(new { success = "true" });
+            }
+            else
+            {
+                return Ok(new { success = "false" });
+            }
+        }
+    }
+}
---
+++ BlogDapperJoaoDias/BlogDapperJoaoDias/Services/CommentService.cs
@@ -0,0 +1,36 @@
+﻿using BlogDapperJoaoDias.Entities;
+using Dapper.Contrib.Extensions;
+using System.Data;
+
+namespace BlogDapperJoaoDias.Services
+{
+    public class CommentService
+    {
+        private IDbConnection _connection;
+        private ConnectionService _connectionService;
+
+        public CommentService(IConfiguration configuration)
+        {
+            _connectionService = new ConnectionService(configuration);
+            _connection = _connectionService.ForDapper();
+        }
+
+        public int Add(Comment comment)
+        {
+            try
+            {
+                var result = _connection.Insert(comment);
+                if (result > 0)
+                {
+                    return int.Parse(result.ToString());
+                }
+
+            }
+            catch (Exception)
+            {
+
+                return 0;
+            }
+        }
+    }
+}
